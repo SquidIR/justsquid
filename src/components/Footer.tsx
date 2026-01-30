9fa9207e5b74fdd293c4ea219bee2babe633cef9
SquidIR <siddithiyer@gmail.com>
2026-01-30 16:30:28 +1100

    Update: Clarify comment for social icons section in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,7 +20,7 @@ const Footer = () => {
             </div>
           </div>
 
-          {/* Social Icons */}
+          {/* Social Icons forever */}
           <div className="flex items-center gap-6">
             <a
               href="https://www.youtube.com/@ItsSquid534"
